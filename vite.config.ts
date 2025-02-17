966dea09387a5608ca616f78f7a1d5bf6b2d08bd
BauerCPP <krasilnikov03@gmail.com>
2025-02-17 17:44:31 +0300

    changed settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-desk-configurator',
   plugins: [react()],
   build: {
     outDir: './dist'
   },
-  publicDir: './public',
+  // publicDir: './public',
     define: {
     __PUBLIC_URL__: process.env.__PUBLIC_URL__,
   },
