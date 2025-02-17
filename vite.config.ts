dcea2e45e2ba84ac929780f6e7225022514f1a33
BauerCPP <krasilnikov03@gmail.com>
2025-02-17 19:17:56 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-desk-configurator',
-  plugins: [react()],
+  base: './',
+  // root: './src',
   build: {
     outDir: './dist'
   },
   publicDir: './public',
-    define: {
-    __PUBLIC_URL__: process.env.__PUBLIC_URL__,
-  },
+  plugins: [react()],
+  assetsInclude: ['**/*.glb']
 })
